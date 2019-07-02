2ab3449f603ed0685a5036d6aeeda8f706e87ed7
nice1st <sasimi86cyh@gmail.com>
2019-07-02 14:16:19 +0900

    인자 병합 변경 - deep

--- js/grid.js
+++ js/grid.js
@@ -110,7 +110,28 @@ FirstGrid.querySearchParent = function(ele, selector) {
   };
 
   this.$list_area = $list_area; // div 영역
-  Object.assign(this.option, option); // 인자 option 과 병합
+  this.option = function mergeDeep(...objects) { // 인자 option 과 병합
+    const isObject = obj => obj && typeof obj === 'object';
+    
+    return objects.reduce((prev, obj) => {
+      Object.keys(obj).forEach(key => {
+        const pVal = prev[key];
+        const oVal = obj[key];
+        
+        if (Array.isArray(pVal) && Array.isArray(oVal)) {
+          prev[key] = pVal.concat(...oVal);
+        }
+        else if (isObject(pVal) && isObject(oVal)) {
+          prev[key] = mergeDeep(pVal, oVal);
+        }
+        else {
+          prev[key] = oVal;
+        }
+      });
+      
+      return prev;
+    }, {});
+  }(this.option, option)
   
   this.init = function() {
     const _this = this;
